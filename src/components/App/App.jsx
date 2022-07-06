a63bc588d26050243ea9690fb848f6fe771f557e
veingardt <beauty-gzhill@mail.ru>
2022-07-06 15:07:50 +0500

    day 1. add package.json and webpack.config.js

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import ToDoLine from '../ToDoLine/ToDoLine.jsx';
+
+const App = () => {
+  return (
+    <div className={'main'}>
+      <h1>To Do List</h1>
+      <input type={'text'} placeholder={'what need to do'} />
+      <ToDoLine />
+      <ToDoLine />
+      <ToDoLine />
+      <ToDoLine />
+    </div>
+  );
+};
+export default App;
---
+++ src/components/ToDoLine/ToDoLine.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ToDoLine.css';
+
+const ToDoLine = () => {
+  return (
+    <div className={'todoline'}>
+      <input type={'checkbox'} />
+      To Do Line
+    </div>
+  );
+};
+
+export default ToDoLine;
